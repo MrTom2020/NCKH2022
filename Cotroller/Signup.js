928b510f61b09d27a1ca251f0f2547f2ec364223
MrTom2020 <tomhumchin2@gmail.com>
2021-11-17 00:09:49 +0700

    make it better

--- Cotroller/Signup.js
+++ Cotroller/Signup.js
@@ -5,4 +5,8 @@ module.exports = function(app)
     app.get("/",function(req,res){
         res.render("login");
     });
+    app.get("/admin",function(req,res)
+    {
+        res.render("login");
+    });
 }

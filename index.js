1ed8416496b413c0a8f8db29c2c885b49d4a51e3
MrTom2020 <tomhumchin2@gmail.com>
2021-11-16 16:42:58 +0700

    make it better

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var express = require("express");
 var app = express();
-
+app.use(express.static("Public"));
+app.set("view engine","ejs");
+app.set("views","./View");
 app.listen(process.env.PORT || 5000);
 
 app.get('/',(req,res)=>{
  res.send("1234");
 });
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+require("./Cotroller/Signup")(app);

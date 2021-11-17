47a360c147e98671e1f36600be47b031435b7cb1
MrTom2020 <tomhumchin2@gmail.com>
2021-11-17 21:48:20 +0700

    make it better

--- Cotroller/Signup.js
+++ Cotroller/Signup.js
@@ -35,6 +35,7 @@ module.exports = function(app)
         }
         else
         {
+            res.json({kq:4,loi:"k0"});
             var u = new user({
                 Name:req.body.Name,
                 Password:req.body.Password

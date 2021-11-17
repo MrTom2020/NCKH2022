22a3d382ee8b91ffee211b64395f38de72421971
MrTom2020 <tomhumchin2@gmail.com>
2021-11-17 19:31:33 +0700

    make it better

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 const mongoose = require('mongoose');
 const { json } = require("body-parser");
-mongoose.connect('mongodb+srv://admin01:hiep1234@cluster0.8kkbk.mongodb.net/Cluster0?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology: true},function(err,db){
-    if(err)
-    {
-        console.log("...11" + err);
-    }
-    else if(db)
-    {
-      console.log("ok123");
-    }
-  });
+// mongoose.connect('mongodb+srv://admin01:hiep1234@cluster0.8kkbk.mongodb.net/Cluster0?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology: true},function(err,db){
+//     if(err)
+//     {
+//         console.log("...11" + err);
+//     }
+//     else if(db)
+//     {
+//       console.log("ok123");
+//     }
+//   });
 require("./Cotroller/Signup")(app);

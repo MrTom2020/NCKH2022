8d730a4643c706aa8c476a64999c4897e79ed017
MrTom2020 <tomhumchin2@gmail.com>
2021-11-19 16:01:57 +0700

    make it better

--- Public/js/homeadmin/main.js
+++ Public/js/homeadmin/main.js
@@ -1,7 +1,19 @@
 $(document).ready(function()
 {
+    var db;
     $.post("../home",function(data)
      {
-         alert(data.loi1.toString());
+        db = data.loi1.toString();
      });
+     var mongoose = require('mongoose');
+     mongoose.connect(db.toString(),{useNewUrlParser:true,useUnifiedTopology: true},function(err,db){
+            if(err)
+            {
+                alert("ko");
+            }
+            else if(db)
+             {
+                alert("ok");
+            }
+            });
 });

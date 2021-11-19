23477585d81abba0c67a351b9d7ce4c0bb78709a
MrTom2020 <tomhumchin2@gmail.com>
2021-11-19 19:57:18 +0700

    make it better

--- Public/js/homeadmin/main.js
+++ Public/js/homeadmin/main.js
@@ -103,6 +103,10 @@ $(document).ready(function()
             });
         }
     });
+    $("#test").click(function(){
+        tableCreate();
+    });
+    
     $("#btnDangKy").click(function()
     {
        if(currentAccount.length == 0)
@@ -146,3 +150,27 @@ function checkBM()
               console.log('Bạn chưa cài MetaMask');
           }
     }
+
+    function tableCreate() {
+        var body = document.getElementsByTagName('body')[0];
+        var tbl = document.createElement('table');
+        tbl.style.width = '100%';
+        tbl.setAttribute('border', '1');
+        var tbdy = document.createElement('tbody');
+        for (var i = 0; i < 3; i++) {
+          var tr = document.createElement('tr');
+          for (var j = 0; j < 2; j++) {
+            if (i == 2 && j == 1) {
+              break
+            } else {
+              var td = document.createElement('td');
+              td.appendChild(document.createTextNode('\u0020'))
+              i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
+              tr.appendChild(td)
+            }
+          }
+          tbdy.appendChild(tr);
+        }
+        tbl.appendChild(tbdy);
+        body.appendChild(tbl)
+      }

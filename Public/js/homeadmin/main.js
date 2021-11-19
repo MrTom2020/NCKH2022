cbca24c7ee6f069cf488b864fd484dee1d192ff8
MrTom2020 <tomhumchin2@gmail.com>
2021-11-20 00:27:16 +0700

    make it better

--- Public/js/homeadmin/main.js
+++ Public/js/homeadmin/main.js
@@ -175,11 +175,11 @@ function checkBM()
         // }
         var fr = document.createElement("form");
         fr.setAttribute('method','post');
-        fr.setAttribute('style','margin-left:15vw');
+        fr.setAttribute('style','margin-left:5vw');
         var ip = document.createElement("input");
         ip.setAttribute('type','text');
         ip.setAttribute('id','ht');
-        ip.setAttribute('style','margin-top:1vw');
+        ip.setAttribute('style','margin-top:2vw');
         ip.setAttribute('class','form-control col-sm-6');
         fr.appendChild(ip);
         myTableDiv.appendChild(fr);

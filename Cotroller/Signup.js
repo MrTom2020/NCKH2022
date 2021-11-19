49810bfc1997b91076e25a39ef9d63a97b5cf052
MrTom2020 <tomhumchin2@gmail.com>
2021-11-19 14:11:16 +0700

    make it better

--- Cotroller/Signup.js
+++ Cotroller/Signup.js
@@ -4,16 +4,6 @@ var kqqq;
 var chuoi;
 module.exports = function(app)
 {
-    mongoose.connect('mongodb+srv://'+ 'admin01' +':'+'hiep1234' + '@cluster0.8kkbk.mongodb.net/Cluster0?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology: true},function(err,db){
-            if(err)
-            {
-                chuoi = "ko";
-            }
-            else if(db)
-             {
-                chuoi = "ok";
-            }
-            });
     app.get("/",function(req,res){
         res.render("login");
     });

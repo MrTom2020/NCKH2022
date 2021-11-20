1726d6c86666c2f108ee070897045baa2c88e955
MrTom2020 <tomhumchin2@gmail.com>
2021-11-20 18:24:50 +0700

    make it better

--- Public/js/homeadmin/main.js
+++ Public/js/homeadmin/main.js
@@ -273,7 +273,7 @@ function checkBM()
 
         var btny = document.createElement("button");
         btny.innerHTML = "Đồng ý";
-        btny.setAttribute('class','btn btn-outline-primary');
+       // btny.setAttribute('class','btn btn-outline-primary');
         btny.setAttribute('id','btny');
         fr.appendChild(btny);
         myTableDiv.appendChild(t);

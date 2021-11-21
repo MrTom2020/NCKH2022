0c6c75fb3a5ff360dd7c04001325903d55e60c43
MrTom2020 <tomhumchin2@gmail.com>
2021-11-21 20:59:16 +0700

    make it better

--- Public/js/homeadmin/main.js
+++ Public/js/homeadmin/main.js
@@ -140,7 +140,7 @@ $(document).ready(function()
     });
     $.post("../home",function(data)
      {
-       alert(JSON.stringify(data.loi1));
+       alert(JSON.stringify(data.loi1.length));
      });
 });
    

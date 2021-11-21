94901515db91c342259d8cf6c9572314bcf283da
MrTom2020 <tomhumchin2@gmail.com>
2021-11-21 16:13:59 +0700

    make it better

--- Cotroller/Insert_data.js
+++ Cotroller/Insert_data.js
@@ -4,6 +4,22 @@ module.exports = function(app)
 {
     app.post('/insertdt',function(req,res)
     {
-        res.json({kq:1});
+        if(!req.body.Email ||
+        !req.body.Password ||
+        !req.body.Name ||
+        !req.body.SDT ||
+        !req.body.BirthDay ||
+        !req.body.CMND ||
+        !req.body.DC ||
+        !req.body.Loai ||
+        !req.body.noitiem ||
+        !req.body.Ngaytiem)
+        {
+            res.json({kq:0});
+        }
+        else
+        {
+            res.json({kq:1});
+        }
     });
 }

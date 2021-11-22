95bfc7577a3750f2c2d5a638c7d4d6075138cd3c
MrTom2020 <tomhumchin2@gmail.com>
2021-11-22 15:56:36 +0700

    make it better

--- Public/js/homeadmin/main.js
+++ Public/js/homeadmin/main.js
@@ -1,70 +1,76 @@
 var dt_User;
 var kt;
 var currentAccount = "";
-var api = [
-    {
-        "anonymous": false,
-        "inputs": [
-            {
-                "indexed": false,
-                "internalType": "address",
-                "name": "_vi",
-                "type": "address"
-            },
-            {
-                "indexed": false,
-                "internalType": "string",
-                "name": "_id",
-                "type": "string"
-            }
-        ],
-        "name": "SM_Ban_Data",
-        "type": "event"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "string",
-                "name": "_id",
-                "type": "string"
-            }
-        ],
-        "name": "Dangky",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "name": "arrHV",
-        "outputs": [
-            {
-                "internalType": "string",
-                "name": "_ID",
-                "type": "string"
-            },
-            {
-                "internalType": "address",
-                "name": "_VI",
-                "type": "address"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    }
-];
-var addressSM = "0x080FC71Dd4dE52eB3916Ee61F59F4Df4887Ce12B";
-var web3 = new Web3(window.ethereum);
-window.ethereum.enable();
-var constract_infura = new web3_infura.eth.Contract(api,addressSM);
 $(document).ready(function()
 {
+    const api = [
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": false,
+                    "internalType": "address",
+                    "name": "_vi",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "string",
+                    "name": "_id",
+                    "type": "string"
+                }
+            ],
+            "name": "SM_Ban_Data",
+            "type": "event"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "string",
+                    "name": "_id",
+                    "type": "string"
+                }
+            ],
+            "name": "Dangky",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "name": "arrHV",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "_ID",
+                    "type": "string"
+                },
+                {
+                    "internalType": "address",
+                    "name": "_VI",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        }
+    ];
+    const addressSM = "0x080FC71Dd4dE52eB3916Ee61F59F4Df4887Ce12B";
+    const web3 = new Web3(window.ethereum);
+    window.ethereum.enable();
+
+    var constract_MM = new web3.eth.Contract(api,addressSM);
+    console.log(constract_MM);
+
+    var provider = new Web3.providers.WebsocketProvider("wss://rinkeby.infura.io/ws/v3/0e394e33cd2c4bb7aec39f9fad35db5c");
+    var web3_infura = new Web3(provider);
+    var constract_infura = new web3_infura.eth.Contract(api,addressSM);
     constract_infura.events.SM_Ban_Data({filter:{},fromBlock:"latest"},function(error,data){
         if(error)
         {

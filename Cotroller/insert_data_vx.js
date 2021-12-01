139c2527a57520e3d0b98f4038232335cc5c5f19
MrTom2020 <tomhumchin2@gmail.com>
2021-12-01 14:49:40 +0700

    make it better

--- Cotroller/insert_data_vx.js
+++ Cotroller/insert_data_vx.js
@@ -2,7 +2,7 @@ var vx = require("../Models/vx");
 
 module.exports = function(app)
 {
-    app.post("../insert_vx",function(req,res){
+    app.post("/insert_vx",function(req,res){
         if(!req.body.Tenvx ||
             !req.body.Loai ||
             !req.body.Noitiem ||

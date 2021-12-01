4e6822de366c4198b6cc2a42f266bcfbac3a422c
MrTom2020 <tomhumchin2@gmail.com>
2021-12-01 16:22:26 +0700

    make it better

--- Public/js/homeadmin/main.js
+++ Public/js/homeadmin/main.js
@@ -72,7 +72,7 @@ $(document).ready(function()
     var provider = new Web3.providers.WebsocketProvider("wss://rinkeby.infura.io/ws/v3/0e394e33cd2c4bb7aec39f9fad35db5c");
     var web3_infura = new Web3(provider);
     var constract_infura = new web3_infura.eth.Contract(api,addressSM);
-    constract_infura.events.SM_Ban_Data({filter:{},fromBlock:"latest"},function(error,data){
+    constract_infura.events.Send({filter:{},fromBlock:"latest"},function(error,data){
         if(error)
         {
             console.log(error + "12344aaaaaaaa");

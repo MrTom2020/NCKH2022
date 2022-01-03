66dfc92cdfcd53d66c8ac060373aa6304ec3591b
MrTom2020 <73622924+MrTom2020@users.noreply.github.com>
2022-01-03 13:09:51 +0700

    make it better

--- Cotroller/Signup.js
+++ Cotroller/Signup.js
@@ -86,11 +86,6 @@ function a(tk,mk)
               if (err) throw err;
               chuoi2 = u;  
             });
-
-         db.collection('users').updateOne({SDT:"99999999"},function(err,res)
-         {
-            kqqq = 0;
-         });
         }
         });
             return kqqq;

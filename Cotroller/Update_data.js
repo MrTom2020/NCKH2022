b57069180c39135ba99aa1fc0565646877b96377
MrTom2020 <73622924+MrTom2020@users.noreply.github.com>
2022-01-08 21:25:33 +0700

    make it better

--- Cotroller/Update_data.js
+++ Cotroller/Update_data.js
@@ -25,7 +25,7 @@ module.exports = function(app)
                 CMND:req.body.CMND,
                 DC:req.body.DC
             });
-            res.json({kq:1,kqtv:"11111111111111111"});
+            res.json({kq:1,kqtv:req.body.Email.toString()});
         }
     });
 }

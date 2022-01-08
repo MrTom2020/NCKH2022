501def8bb3490ef773ef4a2f6460940f594a6b06
MrTom2020 <73622924+MrTom2020@users.noreply.github.com>
2022-01-08 22:25:57 +0700

    make it better

--- Public/js/homeadmin/main.js
+++ Public/js/homeadmin/main.js
@@ -660,7 +660,7 @@ function checkBM()
                }
                else if(data.kq == 1)
                {
-                alert("ok");
+                alert("Cập nhật thông tin thành công");
                }
             //alert(data.kq);
                 // if(data.kq==1)

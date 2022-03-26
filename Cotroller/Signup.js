0fce32bcc98879a7d49a416ff1dded2d2eda7d38
Hiep <73622924+MrTom2020@users.noreply.github.com>
2022-03-26 09:52:38 +0700

    make it better

--- Cotroller/Signup.js
+++ Cotroller/Signup.js
@@ -36,7 +36,7 @@ module.exports = function(app)
             {"firstName":"Anna", "lastName":"Smith"},
             {"firstName":"Peter", "lastName":"Jones"}
           ]
-          res.json(employees);
+          res.send(employees);
     }
     mongoose.connect('mongodb+srv://'+ 'admin01' +':'+'hiep1234' + '@cluster0.8kkbk.mongodb.net/Cluster0?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology: true},function(err,db){
         if(err)

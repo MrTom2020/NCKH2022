2437298d140d56803c2bcce00a6e1395188b2c48
Hiep <73622924+MrTom2020@users.noreply.github.com>
2022-03-26 22:02:01 +0700

    make it better

--- Cotroller/Insert_data.js
+++ Cotroller/Insert_data.js
@@ -4,39 +4,39 @@ module.exports = function(app)
 {
     app.post('/insertdt',function(req,res)
     {
-        if(!req.body.Email ||
-        !req.body.Password ||
-        !req.body.Name ||
-        !req.body.SDT ||
-        !req.body.BirthDay ||
-        !req.body.CMND ||
-        !req.body.DC)
-        {
-            res.json({kq:0,kqtv:"Thiếu tham số"});
-        }
-        else
-        {
-            var u = new user({
-                Email:req.body.Email,
-                Password:req.body.Password,
-                Name:req.body.Name,
-                SDT:req.body.SDT,
-                BirthDay:req.body.BirthDay,
-                CMND:req.body.CMND,
-                DC:req.body.DC
-                //ID_TH:""
-            });
-            u.save(function(error)
-            {
-                if(error)
-                {
-                    res.json({kq:0,kqtv:"Thiếu tham số"});
-                }
-                else
-                {
-                    res.json({kq:1,kqtv:u});
-                }
-            });
-        }
+        // if(!req.body.Email ||
+        // !req.body.Password ||
+        // !req.body.Name ||
+        // !req.body.SDT ||
+        // !req.body.BirthDay ||
+        // !req.body.CMND ||
+        // !req.body.DC)
+        // {
+        //     res.json({kq:0,kqtv:"Thiếu tham số"});
+        // }
+        // else
+        // {
+        //     var u = new user({
+        //         Email:req.body.Email,
+        //         Password:req.body.Password,
+        //         Name:req.body.Name,
+        //         SDT:req.body.SDT,
+        //         BirthDay:req.body.BirthDay,
+        //         CMND:req.body.CMND,
+        //         DC:req.body.DC
+        //         //ID_TH:""
+        //     });
+        //     u.save(function(error)
+        //     {
+        //         if(error)
+        //         {
+        //             res.json({kq:0,kqtv:"Thiếu tham số"});
+        //         }
+        //         else
+        //         {
+        //             res.json({kq:1,kqtv:u});
+        //         }
+        //     });
+        // }
     });
 }

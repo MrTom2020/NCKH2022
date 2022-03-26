19060e3fec81279cc4758d0859b22a6e235693bc
Hiep <73622924+MrTom2020@users.noreply.github.com>
2022-03-26 23:18:12 +0700

    make it better

--- Cotroller/Update_data.js
+++ Cotroller/Update_data.js
@@ -4,36 +4,36 @@ module.exports = function(app)
 {
     app.post('/updatedt',function(req,res)
     {
-        // if(!req.body.Email ||
-        // !req.body.Password ||
-        // !req.body.Name ||
-        // !req.body.SDT ||
-        // !req.body.BirthDay ||
-        // !req.body.CMND ||
-        // !req.body.DC)
-        // {
-        //     res.json({kq:0,kqtv:"Thiếu tham số"});
-        // }
-        // else
-        // {
-        //     var s = req.body.Email;
-        //     var vt = s.indexOf("AAA!!!",0);
-        //     var e = s.substring(0,vt);
-        //     var id = s.substring(vt + 6,s.length);
-        //     user.findOneAndUpdate({_id:id},
-        //     {
-        //         Name:req.body.Name,
-        //    SDT:req.body.SDT,
-        //    BirthDay:req.body.BirthDay,
-        //    CMND:req.body.CMND,
-        //    DC:req.body.DC,
-        //    Password:req.body.Password},(err)=>{
-        //       if(err)
-        //       {
-        //         res.json({kq:0,kqtv:"Thiếu tham số"});
-        //       }
-        //       res.json({kq:1,kqtv:"OK"});
-        //    });
-        // }
+        if(!req.body.Email ||
+        !req.body.Password ||
+        !req.body.Name ||
+        !req.body.SDT ||
+        !req.body.BirthDay ||
+        !req.body.CMND ||
+        !req.body.DC)
+        {
+            res.json({kq:0,kqtv:"Thiếu tham số"});
+        }
+        else
+        {
+            var s = req.body.Email;
+            var vt = s.indexOf("AAA!!!",0);
+            var e = s.substring(0,vt);
+            var id = s.substring(vt + 6,s.length);
+            user.findOneAndUpdate({_id:id},
+            {
+                Name:req.body.Name,
+           SDT:req.body.SDT,
+           BirthDay:req.body.BirthDay,
+           CMND:req.body.CMND,
+           DC:req.body.DC,
+           Password:req.body.Password},(err)=>{
+              if(err)
+              {
+                res.json({kq:0,kqtv:"Thiếu tham số"});
+              }
+              res.json({kq:1,kqtv:"OK"});
+           });
+        }
     });
 }
--- Cotroller/insert_data_vx.js
+++ Cotroller/insert_data_vx.js
@@ -3,33 +3,33 @@ var vx = require("../Models/vx");
 module.exports = function(app)
 {
     app.post("/insert_vx",function(req,res){
-        // if( !req.body.Tenvx ||
-        //     !req.body.Loai ||
-        //     !req.body.Noitiem ||
-        //     !req.body.NgayTiem)
-        //     {
-        //         res.json({kq:0});
-        //     }
-        //     else
-        //     {
-        //         var v = new vx({
-        //             IdUser:req.body.IdUser,
-        //             Tenvx:req.body.Tenvx,
-        //             Loai:req.body.Loai,
-        //             Noitiem:req.body.Noitiem,
-        //             NgayTiem:req.body.NgayTiem   
-        //         });
-        //         v.save(function(error)
-        //         {
-        //             if(error)
-        //             {
-        //                 res.json({kq:0,kqtv_vx:"Error"});
-        //             }
-        //             else
-        //             {
-        //                 res.json({kq:1,kqtv_vx:v});
-        //             }
-        //         });
-        //     }
+        if( !req.body.Tenvx ||
+            !req.body.Loai ||
+            !req.body.Noitiem ||
+            !req.body.NgayTiem)
+            {
+                res.json({kq:0});
+            }
+            else
+            {
+                var v = new vx({
+                    IdUser:req.body.IdUser,
+                    Tenvx:req.body.Tenvx,
+                    Loai:req.body.Loai,
+                    Noitiem:req.body.Noitiem,
+                    NgayTiem:req.body.NgayTiem   
+                });
+                v.save(function(error)
+                {
+                    if(error)
+                    {
+                        res.json({kq:0,kqtv_vx:"Error"});
+                    }
+                    else
+                    {
+                        res.json({kq:1,kqtv_vx:v});
+                    }
+                });
+            }
     });
 }

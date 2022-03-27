854775e9cee52b63744efa7eaf4ca3d475457d3f
Hiep <73622924+MrTom2020@users.noreply.github.com>
2022-03-28 01:11:32 +0700

    make it better\\

---
+++ Cotroller/update.js
@@ -0,0 +1,25 @@
+var user = require("../Models/user");
+
+module.exports = function(app)
+{
+    app.post('/update1',function(req,res)
+    {
+        //     var s = req.body.Email;
+        //     var vt = s.indexOf("AAA!!!",0);
+        //     var e = s.substring(0,vt);
+        //     var id = s.substring(e.length,s.length);
+        //     user.findOneAndUpdate({_id:id},
+        //     {
+        //         ID_TH:e
+        //     },(err)=>{
+        //       if(err)
+        //       {
+        //         res.json({kq:0,kqtv:err});
+        //       }
+        //       else
+        //       {
+        //         res.json({kq:1,kqtv:"OK"});
+        //       }
+        //    });
+    });
+}

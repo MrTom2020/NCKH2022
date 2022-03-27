2b57dfd8f8b01f6ef01952f751af095019b2492d
Hiep <73622924+MrTom2020@users.noreply.github.com>
2022-03-27 23:46:27 +0700

    make it better\

--- Public/js/homeadmin/main.js
+++ Public/js/homeadmin/main.js
@@ -245,7 +245,8 @@ $(document).ready(function()
             //     //console.log(transactionHash);
             //     alert(transaction);
             //   });
-            alert(data.eth.transactionConfirmationBlocks);
+            alert(web3.eth.getTransaction('0x9fc76417374aa880d4449a1f7f31ec597f00b1f6f3dd2d66f4c9c6c445836d8b§234')
+            .then(console.log));
             console.log(error + "ok");
             $.post('../update',{
                 Email:data.returnValues[0] +"AAA!!!" + id

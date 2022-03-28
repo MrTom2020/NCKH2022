ed35d6f187ee7e4766a32004dd6d335ac4a4f9da
Hiep <73622924+MrTom2020@users.noreply.github.com>
2022-03-29 02:15:30 +0700

    make it better

--- Cotroller/Signup.js
+++ Cotroller/Signup.js
@@ -42,7 +42,7 @@ module.exports = function(app)
         user.find({_id:nameUser}).exec(function(err, u)
         {
          if (err) throw err;
-         th:[{u}];
+         th:[u];
          res.send(th);  
        });
     });
